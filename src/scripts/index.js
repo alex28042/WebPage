3f0875eb4b6146ee001e14d81096f9a4d9f959d2
alex28042 <alex28042@gmail.com>
2020-10-21 20:55:42 +0200

    arreglando la pagina

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,9 @@
-
-function change(){
-    var cont = 0;
-    if(cont == 0){
-        document.body.style.backgroundColor = "black";
-        document.getElementById("main-text").style.color = "white";
-        cont = 1;
-    }
-    else{
-        document.body.style.backgroundColor = "white";
-        document.getElementById("main-text").style.color = "#292C35";
-        cont=0;
-    }
+function alerta(){
+    alert("heyy:)")
 }
+
+$(document).ready(function(){
+    $("#arriba").click(function(){
+        $('html, body').animate({scrollToTop:0}, 800);
+    });
+});
